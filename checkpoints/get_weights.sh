fa363b89007dbd4884b8e1db48656420e9e88d1d
Albert Mitjans <a.mitjanscoma@gmail.com>
2020-01-13 09:08:50 +0100

    Modified get_weights

--- checkpoints/get_weights.sh
+++ checkpoints/get_weights.sh
@@ -1,5 +1,9 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=INSERTCODE' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=INSERTCODE" -O data.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lWdxgl10bHCbdYmYfnu7WgDfrY8nOcgT' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lWdxgl10bHCbdYmYfnu7WgDfrY8nOcgT" -O best_ckpt.zip && rm -rf /tmp/cookies.txt
 
-unzip data.zip
+unzip best_ckpt.zip
 
-rm data.zip
+mv best_ckpt/* .
+
+rm -r best ckpt
+
+rm best_ckpt.zip
