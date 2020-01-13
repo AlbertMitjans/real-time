4fb599ce7b5e209fa6313a884e0e87395e55d6af
Albert Mitjans <a.mitjanscoma@gmail.com>
2020-01-13 10:14:52 +0100

    Changes

--- checkpoints/get_weights.sh
+++ checkpoints/get_weights.sh
@@ -1,9 +1,5 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lWdxgl10bHCbdYmYfnu7WgDfrY8nOcgT' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lWdxgl10bHCbdYmYfnu7WgDfrY8nOcgT" -O best_ckpt.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1227yL-vIUhWYH8Idl2hpdiFloxZJUNq6' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1227yL-vIUhWYH8Idl2hpdiFloxZJUNq6" -O best_ckpt.zip && rm -rf /tmp/cookies.txt
 
 unzip best_ckpt.zip
-
-mv best_ckpt/* .
-
-rm -r best ckpt
 
 rm best_ckpt.zip
